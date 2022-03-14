fa7e49854101287ab57f27be1c904d350543825e
JeffAllison <itsjallison@gmail.com>
2022-03-14 16:13:32 -0700

    added refreshcurrentgameel function and displayallgmaes function, added state and functions to event listners

--- app.js
+++ app.js
@@ -11,16 +11,26 @@ const teamTwoSubtractButton = document.getElementById('team-two-subtract-button'
 const finishGameButton = document.getElementById('finish-game-button');
 const teamOneLabel = document.getElementById('team-one-name');
 const teamTwoLabel = document.getElementById('team-two-name');
+const nameOneInput = document.getElementById('team-one');
+const nameTwoInput = document.getElementById('team-two');
 
 // create an array to hold on to the state of past games
 
 let name1 = '';
-let name2 =  '';
+let name2 = '';
 let score1 = 0;
 let score2 = 0;
+let pastGames = [];
 
-nameFormButton.addEventListener('click', (e) => {
+nameFormButton.addEventListener('click', () => {
     // get the name data from the form
+    name1 = nameOneInput.value;
+    name2 = nameTwoInput.value;
+
+    nameOneInput.value = '';
+    nameTwoInput.value = '';
+
+    refreshCurrentGameEl();
 
     // set the state to this data from the form
 
@@ -32,40 +42,46 @@ nameFormButton.addEventListener('click', (e) => {
 
 teamOneAddButton.addEventListener('click', () => {
     // increment the current state for team one's score
-    
+    score1++;
+
     // refresh the current game element with new data by calling the appropriate function
+    refreshCurrentGameEl();
 });
 
 teamTwoAddButton.addEventListener('click', () => {
     // increment the current state for team two's score
+    score2++;
 
     // refresh the current game element with new data by calling the appropriate function
+    refreshCurrentGameEl();
 });
 
 teamOneSubtractButton.addEventListener('click', () => {
     // decrement the current state for team one's score
-
+    score1--;
     // refresh the current game element with new data by calling the appropriate function
+    refreshCurrentGameEl();
 });
 
 teamTwoSubtractButton.addEventListener('click', () => {
     // decrement the current state for team two's score
-
+    score2--;
     // refresh the current game element with new data by calling the appropriate function
+    refreshCurrentGameEl();
 });
 
 finishGameButton.addEventListener('click', () => {
-    
+
     // add the current game to an array of games in state
     // it will be helpful to keep track of these games as objects with 4 properties, one for each piece of state we're tracking
     // for example, make an object like this: { name1: 'ducks', name2: 'bears' ,score1: 1, score2: 2 } 
     // then push it to your array in state
     // (be sure to make a new object. do not declare the object in global scope and mutate it for reuse. This would cause difficult bugs)
-    
+
     displayAllGames();
 
     // reset the state to zero and empty strings
-    
+
     // refresh the current game element with new data by calling the appropriate function
 });
 
@@ -77,9 +93,11 @@ function refreshCurrentGameEl() {
 
     // const gameEl = . . . 
     // make a new gameEl here by calling renderGame with the approriate arguments. 
+    const gameEl = renderGame(name1, name2, score1, score2);
+
     // Check the renderGame function declaration in render-utils.js to figure out the correct arguments to pass to this function 
     // In render-utils.js as yourself: How many arguments does the function take? What order does it take them in?
-    
+
     gameEl.classList.add('current');
 
     currentGameEl.append(gameEl);
@@ -88,8 +106,13 @@ function refreshCurrentGameEl() {
 
 function displayAllGames() {
     // clear out the past games list in the DOM
-
+    pastGamesEl.textContent = '';
     // loop through the past games in state
     // use the renderGame function to render and append a past game for each past game in state
     // again, review the renderGame function in render-utils.js. How many arguments does it take? What order does it take them in?
+    for (let pastGame of pastGames) {
+        const gameEl = renderGame(game);
+        gameEl.classList.add('past');
+        pastGamesEl.append(gameEl);
+    }
 }
